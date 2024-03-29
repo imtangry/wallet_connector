d0fd613250898a0ee78481781f6718a0f44c330e
Rhys <ffffuuuuuxxxx@.f.f>
2024-03-29 15:22:14 +0800

    init project

---
+++ components/Providers.js
@@ -0,0 +1,25 @@
+'use client'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import {WagmiProvider} from 'wagmi'
+import {http, createConfig} from 'wagmi'
+import {mainnet, sepolia} from 'wagmi/chains'
+
+export const config = createConfig({
+    chains: [sepolia],
+    transports: {
+        [mainnet.id]: http(),
+        [sepolia.id]: http(),
+    },
+})
+
+const queryClient = new QueryClient()
+
+export default function Provides({children}) {
+    return (
+        <WagmiProvider config={config}>
+            <QueryClientProvider client={queryClient}>
+                {children}
+            </QueryClientProvider>
+        </WagmiProvider>
+    )
+}
